c0f868bc148a36509f6fba333763ff57d3cbc480
blaze <blazejos1230@gmail.com>
2022-03-20 17:00:09 +0100

    binary chromosome with configured precision

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from src.models.chromosome import Chromosome
+
+chr = Chromosome([-10, 10], 6)
+
+print(chr.binary_arr)
+print(chr.calculate_decimal())
---
+++ src/models/chromosome.py
@@ -0,0 +1,17 @@
+import numpy as np
+import math
+
+
+class Chromosome:
+    def __init__(self, interval=None, precision=None):
+        self.interval = interval
+        self.precision = precision
+        self.binary_arr = np.random.randint(2, size=(self.__calculate_length(),))
+
+    def __calculate_length(self):
+        return math.ceil(math.log2((self.interval[1] - self.interval[0]) * math.pow(10, self.precision)) + math.log2(1))
+
+    def calculate_decimal(self):
+        binary_string = ''.join([str(elem) for elem in self.binary_arr])
+        return self.interval[0] + int(binary_string, 2) * (self.interval[1] - self.interval[0]) / (
+                    math.pow(2, self.binary_arr.size) - 1)
